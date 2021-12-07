81e0c8a526bf298bb700d13778511616d17c45f1
nirav <niravgorasiya10@gmail.com>
2021-12-07 14:43:01 +0530

    project create

---
+++ test.js
@@ -0,0 +1,24 @@
+const deleteCourseCtrl = async(ctrlData)=>{
+    const uni = await getCachedUni(ctrlData.uniUUID)
+    if(uni){
+        const course = await findModelItemQ('Courses',{
+            where:{
+                uniId: uni.uniId,
+                courseUUID: ctrlData.courseUUID
+            }
+        })
+        if(course){
+            const deleteScholarshipAssociations = await deleteModelItemQ('Scholarships_Universities',{
+                where:{
+                    uniId: uni.uniId,
+                    courseId: course.courseId
+                }
+            })
+            return {moreInfo: [{msg: `Course deleted`}]}
+        }else{
+            throw new DataNotFoundError()
+        }
+    }else{
+        throw new DataNotFoundError()
+    }
+}
